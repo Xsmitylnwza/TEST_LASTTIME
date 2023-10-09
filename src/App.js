5969f2dbdffcf4f36f0233b79e90bc1bfdf08d7b
Xsmitylnwza <111405516+Xsmitylnwza@users.noreply.github.com>
2023-10-09 23:22:36 +0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React KUY!!!
+          Learn React KUY!!! KUYYAI HEEYAI
         </a>
         <p>ARAIWA TUMMAI MAIDAI</p>
       </header>
