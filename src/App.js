18ab1135c2f8e6ccceac92755113389e7049a1db
Xsmitylnwza <111405516+Xsmitylnwza@users.noreply.github.com>
2023-10-10 17:10:07 +0700

    v111

--- src/App.js
+++ src/App.js
@@ -11,9 +11,8 @@ function App() {
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+          href="../public/lungtong.html"
+
         >
           Learn React KUY!!! MANGKODYAK EIEIEIEI
 
