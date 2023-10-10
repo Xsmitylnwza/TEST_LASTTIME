b12ae44d93972be6b19b28d0a94c03f7b771c7f2
Xsmitylnwza <111405516+Xsmitylnwza@users.noreply.github.com>
2023-10-10 23:17:18 +0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ function App() {
         </p>
         <a
           className="App-link"
-          href=""
+          href="www.google.com"
 
         >
           Learn React KUY!!! MANGKODYAK EIEIEIEI
